55c9c54dc35084d1c2377942bf8a52435083e414
elbikshfawzy@gmail <elbikshfawzy@gmail.com>
2026-09-09 03:41:21 +0300

    - added images and svg for assets - add flutter_svg and fluter_scereutil - made a file for doc app client - add navigation -added routing folder - added colors and textstyle -added empty login screen

--- lib/features/login/onboarding_screen.dart/onboarding_screen.dart
+++ lib/features/login/onboarding_screen.dart/onboarding_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutterApp/core/theme/styles.dart';
 import 'package:flutterApp/features/login/onboarding_screen.dart/widgets/doc_logo_and_name.dart';
+import 'package:flutterApp/features/login/onboarding_screen.dart/widgets/doctor_image_and_text.dart';
+import 'package:flutterApp/features/login/onboarding_screen.dart/widgets/get_started_button.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnboardingScreen extends StatelessWidget {
@@ -12,7 +15,27 @@ class OnboardingScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.only(top: 30.h, bottom: 30.h),
-            child: const Column(children: [DocLogoAndName()]),
+            child: Column(
+              children: [
+                DocLogoAndName(),
+                SizedBox(height: 30.h),
+                const DoctorImageAndText(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Manage and schedule all of your medical appointments easily with Docdoc to get a new experience.",
+                        textAlign: TextAlign.center,
+                        style: Styles.font13GreyRegular,
+                      ),
+                      SizedBox(height: 10.h),
+                      const GetStartedButton(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
